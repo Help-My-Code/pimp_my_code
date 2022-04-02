030f1278b6e6e03a5265171e71da06523efbe5c1
Swann HERRERA <swann@devloup.dev>
2022-04-02 17:22:36 +0200

    setup archi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'router/router.dart';
+import 'ui/router/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/styles.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class TextStyles {
-  static const bold = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
-}
