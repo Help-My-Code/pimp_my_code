f6a9db9796e427db484c673fdd2bcd03b7d28fd7
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2022-04-06 16:24:27 +0200

    Update router.dart

--- lib/ui/router/router.dart
+++ lib/ui/router/router.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 
@@ -9,39 +10,39 @@ import '../pages/register/register.dart';
 import '../pages/settings.dart';
 
 class PmcRouter {
-  static const title = 'Pimp My Code';
+  static String title = 'title'.tr();
 
   static GoRouter router = GoRouter(
     routes: <GoRoute>[
       GoRoute(
         path: '/',
         builder: (BuildContext context, GoRouterState state) =>
-            const HomePage(title: title),
+            HomePage(title: title),
       ),
       GoRoute(
         path: '/login',
         builder: (BuildContext context, GoRouterState state) =>
-            const LoginPage(title: title),
+            LoginPage(title: title),
       ),
       GoRoute(
         path: '/register',
         builder: (BuildContext context, GoRouterState state) =>
-            const RegisterPage(title: title),
+            RegisterPage(title: title),
       ),
       GoRoute(
         path: '/messaging',
         builder: (BuildContext context, GoRouterState state) =>
-            const MessagingPage(title: title),
+            MessagingPage(title: title),
       ),
       GoRoute(
         path: '/account',
         builder: (BuildContext context, GoRouterState state) =>
-            const AccountPage(title: title),
+            AccountPage(title: title),
       ),
       GoRoute(
         path: '/settings',
         builder: (BuildContext context, GoRouterState state) =>
-            const SettingsPage(title: title),
+            SettingsPage(title: title),
       ),
     ],
   );
