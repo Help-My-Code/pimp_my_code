3a945f5b7b1bc5fb12fc9e431c50b0fc196fcd3b
Florian Gustin <florian.gustin.business@gmail.com>
2022-04-26 23:46:12 +0200

    feat : postcard, imagefullscreenwrapper, codeeditor, codeshowroom implementations to display the post card on home page

---
+++ lib/ui/pages/home/home.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:pimp_my_code/ui/pages/home/widgets/post_card.dart';
+import 'package:pimp_my_code/ui/widgets/code_editor/code_showroom.dart';
+import '../../widgets/app-bar/app_bar_menu.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBarMenu(),
+      body: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            GFButton(
+              onPressed: () {},
+              text: "Ajouter une publication",
+              // icon: Icon(Icons.add),
+              shape: GFButtonShape.square,
+              // type: GFButtonType.transparent,
+            ),
+            Flexible(
+              child: ListView.builder(
+                  itemCount: 28,
+                  itemBuilder: (context, index) {
+                    return PostCard(
+                        onLikePressed: (){},
+                        onUnlikePressed: (){},
+                        onCommentaryPressed: (){},
+                        codes: [
+                          """
+          SafeArea(
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: MaterialButton(
+                padding: const EdgeInsets.all(15),
+                elevation: 0,
+                child: Icon(
+                  Icons.arrow_back,
+                  color: widget.dark ? Colors.white : Colors.black,
+                  size: 25,
+                ),
+                color: widget.dark ? Colors.black12 : Colors.white70,
+                highlightElevation: 0,
+                minWidth: double.minPositive,
+                height: double.minPositive,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ),
+          )
+                          """,
+                        ],
+                        title: "Je suis totalement perdu !!!!",
+                        post: "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).", images: [
+                      'https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg',
+                      'https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg',
+                      'https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg',
+                      'https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg',
+                      'https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg',
+                    ], imageURL: 'https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg', username: 'Benjamin Raynal', date: 'Il y a 5 minutes');
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/widgets/code_editor/code_editor.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+
+class CodeEditor extends StatefulWidget {
+  const CodeEditor({Key? key, required this.onCodeChange, this.initialCode}) : super(key: key);
+  final Function(String?) onCodeChange;
+  final String? initialCode;
+
+  @override
+  State<CodeEditor> createState() => _CodeEditorState();
+}
+
+class _CodeEditorState extends State<CodeEditor> {
+  late TextEditingController textController;
+  String? code;
+
+  @override
+  void initState() {
+    textController = TextEditingController();
+    textController.text = widget.initialCode ?? '';
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  DisableShortcut(
+      child: Actions(
+        actions: {InsertTabIntent: InsertTabAction()},
+        child: Shortcuts(
+          shortcuts: {
+            LogicalKeySet(LogicalKeyboardKey.tab):
+            InsertTabIntent(2, textController),
+          },
+          child: TextField(
+            onChanged: (v) {
+              setState(() {
+                code = v;
+              });
+              widget.onCodeChange(v);
+            },
+            enableInteractiveSelection: true,
+            toolbarOptions: ToolbarOptions(
+              copy: true,
+              cut: true,
+              paste: true,
+              selectAll: true,
+            ),
+            style: TextStyle(fontWeight: FontWeight.bold),
+            controller: textController,
+            textInputAction: TextInputAction.newline,
+            maxLines: 30,
+            keyboardType: TextInputType.multiline,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InsertTabIntent extends Intent {
+  const InsertTabIntent(this.numSpaces, this.textController);
+
+  final int numSpaces;
+  final TextEditingController textController;
+}
+
+class InsertTabAction extends Action {
+  @override
+  Object invoke(covariant Intent intent) {
+    if (intent is InsertTabIntent) {
+      final oldValue = intent.textController.value;
+      final newComposing = TextRange.collapsed(oldValue.composing.start);
+      final newSelection = TextSelection.collapsed(
+          offset: oldValue.selection.start + intent.numSpaces);
+
+      final newText = StringBuffer(oldValue.selection.isValid
+          ? oldValue.selection.textBefore(oldValue.text)
+          : oldValue.text);
+      for (var i = 0; i < intent.numSpaces; i++) {
+        newText.write(' ');
+      }
+      newText.write(oldValue.selection.isValid
+          ? oldValue.selection.textAfter(oldValue.text)
+          : '');
+      intent.textController.value = intent.textController.value.copyWith(
+        composing: newComposing,
+        text: newText.toString(),
+        selection: newSelection,
+      );
+    }
+    return '';
+  }
+}
+
+class InsertRevertIntent extends Intent {
+  const InsertRevertIntent(this.numSpaces, this.textController);
+
+  final int numSpaces;
+  final TextEditingController textController;
+}
+
+class InsertRevertAction extends Action {
+  @override
+  Object invoke(covariant Intent intent) {
+    if (intent is InsertRevertIntent) {
+      intent.textController.value = intent.textController.value.copyWith(
+        text: intent.textController.value.text,
+      );
+    }
+    return '';
+  }
+}
+
+///* for mac replace  LogicalKeyboardKey.control, with LogicalKeyboardKey.meta
+final selectableKeySetwindows = LogicalKeySet(
+  LogicalKeyboardKey.control,
+  LogicalKeyboardKey.keyA,
+);
+final pasteKeySetwindows = LogicalKeySet(
+  LogicalKeyboardKey.control,
+  LogicalKeyboardKey.keyV,
+);
+
+/// i dont have any ios device 😅,let me know what it produce.
+final selectableKeySetMac = LogicalKeySet(
+  LogicalKeyboardKey.meta,
+  LogicalKeyboardKey.keyA,
+);
+
+class DisableShortcut extends StatelessWidget {
+  final Widget child;
+
+  const DisableShortcut({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FocusableActionDetector(
+      shortcuts: {
+        selectableKeySetwindows: SelectionIntent(),
+        pasteKeySetwindows: PasteIntent(),
+      },
+      actions: {
+        SelectionIntent: CallbackAction(
+          onInvoke: (intent) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("Copy is forbidden")));
+            return FocusScope.of(context).requestFocus(FocusNode());
+          },
+        ),
+        PasteIntent: CallbackAction(
+          onInvoke: (intent) async {
+            // ClipboardData? data = await Clipboard.getData('text/plain');
+            // print(" paste callBack ${data!.text}");
+            return ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("Paste is forbidden")));
+          },
+        )
+      },
+      autofocus: true,
+      child: child,
+    );
+  }
+}
+
+class SelectionIntent extends Intent {}
+
+class PasteIntent extends Intent {}
