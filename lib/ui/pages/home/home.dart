2f86f53c4a8016aab80c58216f8a778bd8150254
Swann HERRERA <swann@devloup.dev>
2022-04-28 00:31:52 +0200

    load product on initial state

--- lib/ui/pages/home/home.dart
+++ lib/ui/pages/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../state/retrieve_content/retrieve_content_cubit.dart';
@@ -12,10 +13,25 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBarMenu(),
-      body: BlocBuilder<RetrieveContentCubit, RetrieveContentState>(
+      body: BlocConsumer<RetrieveContentCubit, RetrieveContentState>(
+        listener: (context, state) {
+          state.maybeWhen(
+            orElse: () {},
+            failure: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('Failed_to_load_publications').tr(),
+                ),
+              );
+            },
+          );
+        },
         builder: (context, state) {
-          context.read<RetrieveContentCubit>().loadPublication();
           return state.maybeWhen(
+            initial: () {
+              context.read<RetrieveContentCubit>().loadPublication();
+              return const Loading();
+            },
             orElse: () => const Loading(),
             loaded: (publications) => const HomeLoaded(),
           );
