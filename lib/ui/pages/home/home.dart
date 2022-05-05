889609f8adfab21a986c2404e7dfb444cfb85893
Florian Gustin <florian.gustin.business@gmail.com>
2022-05-05 14:50:56 +0200

    fix : layout home

--- lib/ui/pages/home/home.dart
+++ lib/ui/pages/home/home.dart
@@ -6,8 +6,11 @@ import 'package:getwidget/components/button/gf_button.dart';
 import 'package:getwidget/shape/gf_button_shape.dart';
 import 'package:pimp_my_code/state/post/create_post_cubit.dart';
 import 'package:pimp_my_code/ui/pages/home/widgets/create_post_card.dart';
+import 'package:pimp_my_code/ui/pages/home/widgets/home_loaded.dart';
 
+import '../../../state/retrieve_content/retrieve_content_cubit.dart';
 import '../../widgets/app-bar/app_bar_menu.dart';
+import '../../widgets/loading.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
@@ -40,31 +43,34 @@ class HomePage extends StatelessWidget {
                 ),
               );
             }),
-            // BlocConsumer<RetrieveContentCubit, RetrieveContentState>(
-            //   listener: (context, state) {
-            //     state.maybeWhen(
-            //       orElse: () {},
-            //       failure: () {
-            //         ScaffoldMessenger.of(context).showSnackBar(
-            //           SnackBar(
-            //             content: const Text('Failed_to_load_publications').tr(),
-            //           ),
-            //         );
-            //       },
-            //     );
-            //   },
-            //   builder: (context, state) {
-            //     return state.maybeWhen(
-            //       initial: () {
-            //         context.read<RetrieveContentCubit>().loadPublication();
-            //         return const Loading();
-            //       },
-            //       orElse: () => const Loading(),
-            //       loaded: (publications) =>
-            //           HomeLoaded(publications: publications),
-            //     );
-            //   },
-            // ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: BlocConsumer<RetrieveContentCubit, RetrieveContentState>(
+                listener: (context, state) {
+                  state.maybeWhen(
+                    orElse: () {},
+                    failure: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: const Text('Failed_to_load_publications').tr(),
+                        ),
+                      );
+                    },
+                  );
+                },
+                builder: (context, state) {
+                  return state.maybeWhen(
+                    initial: () {
+                      context.read<RetrieveContentCubit>().loadPublication();
+                      return const Loading();
+                    },
+                    orElse: () => const Loading(),
+                    loaded: (publications) =>
+                        HomeLoaded(publications: publications),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
